6a65ac2d50e9d2d41ccad04d425a40e207d6050e
Facundo Falcone <amilcar.f.falcone@accenture.com>
2022-03-28 22:34:31 -0300

    Update :scroll: - License code fixed

--- Modules/Data_Validator_Mod/data_validator.py
+++ Modules/Data_Validator_Mod/data_validator.py
@@ -15,7 +15,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 
 def validate_answer(message: str, test_validate: str = None) -> bool:
     """[summary]\n
--- structure_builder.py
+++ structure_builder.py
@@ -15,7 +15,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 
 from Modules.Entities_Mod.structure import Structure
 from Modules.Auxiliars.data_collector import structure_name_col
